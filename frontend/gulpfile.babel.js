e94dbf2e303985e6d320b5b0ae39589397019ddf
Bartek Mis <bartek@studiosidekicks.com>
2017-11-06 12:51:22 +0000

    gulpfile.babel.js edited online with Bitbucket

--- frontend/gulpfile.babel.js
+++ frontend/gulpfile.babel.js
@@ -157,11 +157,7 @@ gulp.task('styles', () => {
   ];
 
   // For best performance, don't add Sass partials to `gulp.src`
-  return gulp.src([
-    // 'app/styles/**/*.scss',
-    // 'app/styles/**/*.css'
-    'app/styles/*.scss'
-  ])
+  return gulp.src('app/styles/*.scss')
     .pipe($.newer('.tmp/styles'))
     .pipe($.sourcemaps.init())
     .pipe($.sass({
