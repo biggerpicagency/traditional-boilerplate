f358d3fffe34f0db3b3f7067533e62c9c1cc6884
Bartek Mis <bartek@studiosidekicks.com>
2019-01-27 21:55:15 +0100

    small tweaks

--- frontend/app/scripts/utils/hash-links-scroll.js
+++ frontend/app/scripts/utils/hash-links-scroll.js
@@ -6,14 +6,15 @@ const hashLinkScroll = () => {
     const hashLinks = document.querySelectorAll('a[href^="#"]:not(.js-scroll-to)');
     
     hashLinks.forEach((link) => {
-        link.addEventListener('click', function() {
+        link.addEventListener('click', function(event) {
             let target = this.hash;
             let hashValue = target.substr(target.indexOf("#"));
 
             if (hashValue.length) {
                 scrollToId({id: hashValue});
             }
-            
+
+            event.preventDefault();
             return false;
         });
     });
